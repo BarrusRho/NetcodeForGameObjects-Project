94085ddac6fa465cfe543e9a1ea786de98060c28
BarrusRho <60799948+BarrusRho@users.noreply.github.com>
2023-07-14 16:59:57 +0300

    - Updates shooting mechanics to use coins as a way of limiting spamming the shoot button.  Also implements a timer over using Time.Time

--- Assets/Scripts/Core/Coin/CoinWallet.cs
+++ Assets/Scripts/Core/Coin/CoinWallet.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
@@ -8,6 +9,11 @@ namespace NetcodeForGameObjects.Core
     public class CoinWallet : NetworkBehaviour
     {
         public NetworkVariable<int> TotalCoins = new NetworkVariable<int>();
+
+        public void SpendCoins(int costToFire)
+        {
+            TotalCoins.Value -= costToFire;
+        }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
--- Assets/Scripts/Core/Player/PlayerShooting.cs
+++ Assets/Scripts/Core/Player/PlayerShooting.cs
@@ -11,9 +11,8 @@ namespace NetcodeForGameObjects.Core
     {
         [Header("References")]
         [SerializeField] private InputManager _inputManager;
-
+        [SerializeField] private CoinWallet _coinWallet;
         [SerializeField] private Collider2D _playerCollider;
-
         [SerializeField] private GameObject _serverProjectilePrefab;
         [SerializeField] private GameObject _clientProjectilePrefab;
         [SerializeField] private Transform _projectileSpawnPoint;
@@ -23,9 +22,10 @@ namespace NetcodeForGameObjects.Core
         [SerializeField] private float _fireRate;
         [SerializeField] private float _projectileSpeed;
         [SerializeField] private float _muzzleFlashDuration;
+        [SerializeField] private int _costToFire = 1;
 
         private bool _canShoot;
-        private float _fireRateTimer;
+        private float _gameTimer;
         private float _muzzleFlashTimer;
 
         public override void OnNetworkSpawn()
@@ -64,12 +64,22 @@ namespace NetcodeForGameObjects.Core
                 return;
             }
 
+            if (_gameTimer > 0f)
+            {
+                _gameTimer -= Time.deltaTime;
+            }
+
             if (!_canShoot)
             {
                 return;
             }
 
-            if (Time.time < (1 / _fireRate) + _fireRateTimer)
+            if (_gameTimer > 0f)
+            {
+                return;
+            }
+
+            if (_coinWallet.TotalCoins.Value < _costToFire)
             {
                 return;
             }
@@ -78,7 +88,7 @@ namespace NetcodeForGameObjects.Core
 
             SpawnDummyProjectile(_projectileSpawnPoint.position, _projectileSpawnPoint.up);
 
-            _fireRateTimer = Time.time;
+            _gameTimer = 1 / _fireRate;
         }
 
         private void OnPrimaryFire(bool canShoot)
@@ -89,6 +99,13 @@ namespace NetcodeForGameObjects.Core
         [ServerRpc]
         private void PrimaryFireServerRpc(Vector3 spawnPosition, Vector3 projectileDirection)
         {
+            if (_coinWallet.TotalCoins.Value < _costToFire)
+            {
+                return;
+            }
+
+            _coinWallet.SpendCoins(_costToFire);
+
             var projectile = Instantiate(_serverProjectilePrefab, spawnPosition, Quaternion.identity);
             projectile.transform.up = projectileDirection;
 

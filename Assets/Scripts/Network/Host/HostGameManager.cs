ad70fd7096538ccd68d6dab895e4091fc53763dd
BarrusRho <60799948+BarrusRho@users.noreply.github.com>
2023-08-05 17:45:10 +0300

    - Adds functionality for creating lobbies

--- Assets/Scripts/Network/Host/HostGameManager.cs
+++ Assets/Scripts/Network/Host/HostGameManager.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using Unity.Networking.Transport.Relay;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
 using Unity.Services.Relay;
 using Unity.Services.Relay.Models;
 using UnityEngine;
@@ -16,6 +18,7 @@ namespace NetcodeForGameObjects.Network
     {
         private Allocation _allocation;
         private string _joinCode;
+        private string _lobbyId;
         private const int _maxConnections = 20;
 
         private const string _gameSceneName = "Main";
@@ -47,9 +50,42 @@ namespace NetcodeForGameObjects.Network
             var relayServerData = new RelayServerData(_allocation, "dtls");
             unityTransport.SetRelayServerData(relayServerData);
 
+            try
+            {
+                var lobbyOptions = new CreateLobbyOptions();
+                lobbyOptions.IsPrivate = false;
+                lobbyOptions.Data = new Dictionary<string, DataObject>()
+                {
+                    {
+                        "JoinCode", new DataObject(visibility: DataObject.VisibilityOptions.Member, value: _joinCode)
+                    }
+                };
+
+                var lobby = await Lobbies.Instance.CreateLobbyAsync("My Lobby", _maxConnections, lobbyOptions);
+                _lobbyId = lobby.Id;
+
+                HostSingleton.Instance.StartCoroutine(HeartbeatLobbyRoutine(15));
+            }
+            catch (LobbyServiceException exception)
+            {
+                Debug.Log(exception);
+                return;
+            }
+
             NetworkManager.Singleton.StartHost();
 
             NetworkManager.Singleton.SceneManager.LoadScene(_gameSceneName, LoadSceneMode.Single);
+        }
+
+        private IEnumerator HeartbeatLobbyRoutine(float waitTimeSeconds)
+        {
+            var delay = new WaitForSecondsRealtime(waitTimeSeconds);
+
+            while (true)
+            {
+                Lobbies.Instance.SendHeartbeatPingAsync(_lobbyId);
+                yield return delay;
+            }
         }
     }
 }

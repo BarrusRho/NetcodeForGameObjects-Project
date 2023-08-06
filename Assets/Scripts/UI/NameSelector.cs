25c3d9a7b2756982fec6e98cbb2ce634106cd1e1
BarrusRho <60799948+BarrusRho@users.noreply.github.com>
2023-08-07 01:55:58 +0300

    - Adds functionality for validating a PlayerName change

---
+++ Assets/Scripts/UI/NameSelector.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using SystemInfo = UnityEngine.Device.SystemInfo;
+
+namespace NetcodeForGameObjects.UI
+{
+    public class NameSelector : MonoBehaviour
+    {
+        [SerializeField] private TMP_InputField _nameField;
+        [SerializeField] private Button _connectButton;
+        [SerializeField] private int _minNameLength = 1;
+        [SerializeField] private int _maxNameLength = 12;
+
+        private const string PlayerNameKey = "PlayerName";
+        
+        private void Start()
+        {
+            if (SystemInfo.graphicsDeviceType == UnityEngine.Rendering.GraphicsDeviceType.Null)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                return;
+            }
+            
+            _nameField.text = PlayerPrefs.GetString(PlayerNameKey, string.Empty);
+            ValidateNameChange();
+        }
+
+        public void ValidateNameChange()
+        {
+            _connectButton.interactable =
+                _nameField.text.Length >= _minNameLength && _nameField.text.Length <= _maxNameLength;
+        }
+
+        public void Connect()
+        {
+            PlayerPrefs.SetString(PlayerNameKey, _nameField.text);
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+    }
+}
